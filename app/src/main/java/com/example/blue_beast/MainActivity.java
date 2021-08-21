ec7f1d059f5290bb317339a3e68865048fd4ad1b
TanishqBhardwaj <47564288+TanishqBhardwaj@users.noreply.github.com>
2021-08-22 00:28:40 +0530

    Add basic UI of student record

--- app/src/main/java/com/example/blue_beast/MainActivity.java
+++ app/src/main/java/com/example/blue_beast/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.blue_beast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setView();
+        setListeners();
+    }
+
+    private void setView() {
+        tabLayout = findViewById(R.id.tab_layout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("PERSONAL DETAILS"));
+        tabLayout.addTab(tabLayout.newTab().setText("PARENTS & GUARDIAN"));
+        tabLayout.addTab(tabLayout.newTab().setText("ID PROOF"));
+        tabLayout.addTab(tabLayout.newTab().setText("CERTIFICATE"));
+    }
+
+    private void setListeners() {
+        tabLayout.setOnClickListener(view -> {
+
+        });
     }
 }
